65ac98bc18a5c1819c97d6d89efed47b9068b9a4
Nicole Venner <nvenner@protonmail.ch>
2024-07-02 23:35:13 -0600

    add types and fix compilation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,60 @@
 import Image from "next/image";
 import { useAsciiText, alligator } from "react-ascii-text";
 
+import { ReactNode } from "react";
 import TextArt from "@/components/AsciiText";
 
 import AsciiArtHTML from "@/components/AsciiArtHTML";
 
 // Paragraph Component
-const Paragraph = ({ children }) => <p className="mb-4">{children}</p>;
+interface ParagraphProps {
+  children: ReactNode;
+}
+const Paragraph: React.FC<ParagraphProps> = ({ children }) => (
+  <p className="mb-4">{children}</p>
+);
 
 // List Component
-const List = ({ children }) => (
+interface ListProps {
+  children: ReactNode[];
+}
+const List: React.FC<ListProps> = ({ children }) => (
   <ul className="list-disc list-inside mb-4">{children}</ul>
 );
 
 // ListItem Component
-const ListItem = ({ children }) => <li className="mb-2">{children}</li>;
+interface ListItemProps {
+  children: ReactNode;
+}
+const ListItem: React.FC<ListItemProps> = ({ children }) => (
+  <li className="mb-2">{children}</li>
+);
 
 // Table Component
-const Table = ({ children }) => (
+interface TableProps {
+  children: ReactNode[];
+}
+const Table: React.FC<TableProps> = ({ children }) => (
   <table className="table-auto w-full mb-4">{children}</table>
 );
 
 // TableRow Component
-const TableRow = ({ children }) => <tr>{children}</tr>;
+interface TableRowProps {
+  children: ReactNode[];
+}
+const TableRow: React.FC<TableRowProps> = ({ children }) => <tr>{children}</tr>;
 
 // TableCell Component
-const TableCell = ({ children, header }) => (
+interface TableCellProps {
+  children: ReactNode;
+  header?: boolean;
+}
+const TableCell: React.FC<TableCellProps> = ({ children, header }) => (
   <td className={`border px-4 py-2 ${header ? "font-bold" : ""}`}>
     {children}
   </td>
 );
+
 //  <main className="flex min-h-screen flex-col items-center justify-between p-24">
 //    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 const MainComponent = () => (
@@ -41,8 +66,8 @@ const MainComponent = () => (
         <Paragraph>
           Kessler is a search application that aims to take in every single
           document produced by the US government, from the National
-          Transportation Safety Administration's Report on the Baltimore Bridge
-          Collapse, to the public comments on Thornton, CO's recent zoning
+          Transportation Safety Administration Report on the Baltimore Bridge
+          Collapse, to the public comments on Thornton, CO recent zoning
           changes.
         </Paragraph>
         <Paragraph>
@@ -62,15 +87,15 @@ const MainComponent = () => (
           </ListItem>
           <ListItem>Full Text Search Across all ingested documents</ListItem>
           <ListItem>
-            Use of a chatbot to interact with the docket using LLM's and RAG
+            Use of a chatbot to interact with the docket using LLMs and RAG
           </ListItem>
           <ListItem>
             Ability to mass upload your own documents and integrate with all our
             existing features
           </ListItem>
           <ListItem>
-            All within a User Interface that doesn't look like it was designed
-            in the 1990's
+            All within a User Interface that doesn&apos;t look like it was
+            designed in the 1990s
           </ListItem>
         </List>
         <TextArt label="Try Now" text="try_now" url="https://app.kessler.xyz" />
@@ -90,7 +115,7 @@ const MainComponent = () => (
                 <ListItem>
                   Access to a limited set of open access government documents
                 </ListItem>
-                <ListItem>Use of open source LLM's</ListItem>
+                <ListItem>Use of open source LLMs</ListItem>
                 <ListItem>
                   Run your own instance with our open source code at
                   (git.mycor.io/mycorrhiza/kessler)
@@ -106,7 +131,7 @@ const MainComponent = () => (
                 <ListItem>
                   Ability to upload and process your own documents
                 </ListItem>
-                <ListItem>Smarter LLM's for chatbots</ListItem>
+                <ListItem>Smarter LLMs for chatbots</ListItem>
                 <ListItem>Everything in Community</ListItem>
               </List>
             </TableCell>
