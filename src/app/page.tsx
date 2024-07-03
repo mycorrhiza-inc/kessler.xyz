409235a8c5550bd3f5836208e3ffd73d7a67937e
Nicole Venner <nvenner@protonmail.ch>
2024-07-02 23:24:44 -0600

    good enough

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,103 +30,108 @@ const TableCell = ({ children, header }) => (
     {children}
   </td>
 );
+//  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 const MainComponent = () => (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <AsciiArtHTML htmlPath="/home/nicole/Documents/mycorrhizae/kessler.xyz/src/assets/ascii_logo_medium.txt" />
-      <TextArt label="Kessler" text="kess_logo" />
-      <Paragraph>
-        Kessler is a search application that aims to take in every single
-        document produced by the US government, from the National Transportation
-        Safety Administration's Report on the Baltimore Bridge Collapse, to the
-        public comments on Thornton, CO's recent zoning changes.
-      </Paragraph>
-      <Paragraph>
-        Currently the product is still in early alpha, providing a list of
-        features:
-      </Paragraph>
-      <List>
-        <ListItem>
-          A large list of government documents (over 100k in total)
-        </ListItem>
-        <ListItem>
-          Identical search features to most government dockets out there (Search
-          by Title, Filter by Author, etc...)
-        </ListItem>
-        <ListItem>
-          Ability to ingest PDFs/Audio Recordings/Word Docs/Ebooks
-        </ListItem>
-        <ListItem>Full Text Search Across all ingested documents</ListItem>
-        <ListItem>
-          Use of a chatbot to interact with the docket using LLM's and RAG
-        </ListItem>
-        <ListItem>
-          Ability to mass upload your own documents and integrate with all our
-          existing features
-        </ListItem>
-        <ListItem>
-          All within a User Interface that doesn't look like it was designed in
-          the 1990's
-        </ListItem>
-      </List>
-      <TextArt label="Try Now" text="try_now" url="https://app.kessler.xyz" />
-      <br />
-      <TextArt label="Book Demo" text="book_demo" url="/contact" />
-      <Paragraph>We have multiple pricing options available:</Paragraph>
-      <Table>
-        <TableRow>
-          <TableCell header>Tier</TableCell>
-          <TableCell header>Features</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Community Tier</TableCell>
-          <TableCell>
-            <List>
-              <ListItem>Access to all search functionality</ListItem>
-              <ListItem>
-                Access to a limited set of open access government documents
-              </ListItem>
-              <ListItem>Use of open source LLM's</ListItem>
-              <ListItem>
-                Run your own instance with our open source code at
-                (git.mycor.io/mycorrhiza/kessler)
-              </ListItem>
-            </List>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Professional Tier ($20 per month)</TableCell>
-          <TableCell>
-            <List>
-              <ListItem>Access to all our government documents</ListItem>
-              <ListItem>
-                Ability to upload and process your own documents
-              </ListItem>
-              <ListItem>Smarter LLM's for chatbots</ListItem>
-              <ListItem>Everything in Community</ListItem>
-            </List>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>Enterprise Tier (Contact Us)</TableCell>
-          <TableCell>
-            <List>
-              <ListItem>
-                Priority support for adding your custom datasets
-              </ListItem>
-              <ListItem>Everything in Professional</ListItem>
-            </List>
-          </TableCell>
-        </TableRow>
-      </Table>
-      <Paragraph>
-        Made with{" "}
-        <span role="img" aria-label="heart">
-          ❤️
-        </span>{" "}
-        by
-      </Paragraph>
-      <TextArt label="Mycorrhiza" text="mycorrhiza_logo" />
+    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
+      <>
+        <AsciiArtHTML htmlPath="/home/nicole/Documents/mycorrhizae/kessler.xyz/src/assets/ascii_logo_medium.txt" />
+        <TextArt label="Kessler" text="kess_logo" />
+        <Paragraph>
+          Kessler is a search application that aims to take in every single
+          document produced by the US government, from the National
+          Transportation Safety Administration's Report on the Baltimore Bridge
+          Collapse, to the public comments on Thornton, CO's recent zoning
+          changes.
+        </Paragraph>
+        <Paragraph>
+          Currently the product is still in early alpha, providing a list of
+          features:
+        </Paragraph>
+        <List>
+          <ListItem>
+            A large list of government documents (over 100k in total)
+          </ListItem>
+          <ListItem>
+            Identical search features to most government dockets out there
+            (Search by Title, Filter by Author, etc...)
+          </ListItem>
+          <ListItem>
+            Ability to ingest PDFs/Audio Recordings/Word Docs/Ebooks
+          </ListItem>
+          <ListItem>Full Text Search Across all ingested documents</ListItem>
+          <ListItem>
+            Use of a chatbot to interact with the docket using LLM's and RAG
+          </ListItem>
+          <ListItem>
+            Ability to mass upload your own documents and integrate with all our
+            existing features
+          </ListItem>
+          <ListItem>
+            All within a User Interface that doesn't look like it was designed
+            in the 1990's
+          </ListItem>
+        </List>
+        <TextArt label="Try Now" text="try_now" url="https://app.kessler.xyz" />
+        <br />
+        <TextArt label="Book Demo" text="book_demo" url="/contact" />
+        <Paragraph>We have multiple pricing options available:</Paragraph>
+        <Table>
+          <TableRow>
+            <TableCell header>Tier</TableCell>
+            <TableCell header>Features</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Community Tier</TableCell>
+            <TableCell>
+              <List>
+                <ListItem>Access to all search functionality</ListItem>
+                <ListItem>
+                  Access to a limited set of open access government documents
+                </ListItem>
+                <ListItem>Use of open source LLM's</ListItem>
+                <ListItem>
+                  Run your own instance with our open source code at
+                  (git.mycor.io/mycorrhiza/kessler)
+                </ListItem>
+              </List>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Professional Tier ($20 per month)</TableCell>
+            <TableCell>
+              <List>
+                <ListItem>Access to all our government documents</ListItem>
+                <ListItem>
+                  Ability to upload and process your own documents
+                </ListItem>
+                <ListItem>Smarter LLM's for chatbots</ListItem>
+                <ListItem>Everything in Community</ListItem>
+              </List>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>Enterprise Tier (Contact Us)</TableCell>
+            <TableCell>
+              <List>
+                <ListItem>
+                  Priority support for adding your custom datasets
+                </ListItem>
+                <ListItem>Everything in Professional</ListItem>
+              </List>
+            </TableCell>
+          </TableRow>
+        </Table>
+        <Paragraph>
+          Made with{" "}
+          <span role="img" aria-label="heart">
+            ❤️
+          </span>{" "}
+          by
+        </Paragraph>
+        <TextArt label="Mycorrhiza" text="mycorrhiza_logo" />
+      </>
     </div>
   </main>
 );
