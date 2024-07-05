b290e80019830347066184c3065e1d14e2316cdd
Nicole Venner <nvenner@protonmail.ch>
2024-07-05 08:02:08 -0600

    css difficult

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        "3xs": "0.25rem",
+        "2xs": "0.375rem",
+        xs: "0.5rem",
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
