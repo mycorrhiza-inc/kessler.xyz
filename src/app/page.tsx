4a134e2293f4d6669abef1e0d4c24631c03a5ba2
Nicole Venner <nvenner@protonmail.ch>
2024-07-14 12:00:11 -0600

    add source button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,6 +112,17 @@ const MainComponent = () => (
             </span>
           </button>
         </a>
+        <Paragraph>
+          <br />
+        </Paragraph>
+        <a href="https://github.com/mycorrhiza-inc/kessler">
+          <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Source Code
+            </span>
+          </button>
+        </a>
+
 
         <Paragraph>
           We really want to continue building out this software, and also keep
